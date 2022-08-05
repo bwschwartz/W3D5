e5b83efb3b0768c654a27b2b1c329327b07b9cc4
heatherpark201 <heatherpark201@gmail.com>
2022-08-05 12:04:42 -0400

    Initialize PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,35 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+    attr_writer :children
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(node)
+        if node == nil 
+            @parent = nil 
+            return
+        end
+
+        if parent 
+            @parent.children.delete(self)
+        end
+
+        @parent = node
+        node.children << self
+    end
+
+
 end
+
+n1 = PolyTreeNode.new('root1')
+n2 = PolyTreeNode.new('root2')
+n3 = PolyTreeNode.new('root3')
+
+
+
+
