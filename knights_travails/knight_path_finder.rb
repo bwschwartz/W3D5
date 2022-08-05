8bcfd06abb17e27952712566ccbfc1ec587c942a
heatherpark201 <heatherpark201@gmail.com>
2022-08-05 15:44:37 -0400

    define valid_moves class method

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,8 +1,24 @@
 class KnightPathFinder
 
   def self.valid_moves(pos)
-    available_pos = []
-    new_pos = pos +
+    moves = 
+    [[1, 2], 
+    [-1, 2], 
+    [-1, -2], 
+    [1, -2], 
+    [2, 1], 
+    [2, -1], 
+    [-2, -1], 
+    [-2, 1]]
+
+    valid_pos = []
+
+    moves.each do |move|
+      potent_move = move.map.with_index { |num, i| num + pos[i] }
+      valid_pos << potent_move if valid?(potent_move)
+    end
+
+    valid_pos
   end
 
 
@@ -18,7 +34,7 @@ class KnightPathFinder
   end
 
   private
-  def within_bounds(pos)
-    pos.each { |i| i <=0 && i }
+  def self.valid?(pos)
+    pos.all? { |i| i >= 0 && i < 8 }
   end
 end
